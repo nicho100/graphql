83c2551629fc237c7b56f169adc576bc628f3aaa
nicho100 <63875333+nicho100@users.noreply.github.com>
2023-05-02 18:52:54 -0300

    Update queries.js

--- graphql/resolver.js/users/queries.js
+++ graphql/resolver.js/users/queries.js
@@ -1,6 +1,6 @@
 const { getUsers, getById } = require("../../../controller/user");
 
-module.exports.userQueries = {
+module.exports.userQueries = {//creo la queri pra recibir todos los usuarios y para recibir uno solo cuando recibe un id
   users: async () => await getUsers(),
   user: async (_, { userId }) => await getById(userId),
 };
