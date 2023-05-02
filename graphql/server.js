a4aa68e5ccbd25600bd3a776c60b9ebf76d28709
nicho100 <63875333+nicho100@users.noreply.github.com>
2023-05-02 18:39:48 -0300

    Update server.js

--- graphql/server.js
+++ graphql/server.js
@@ -2,7 +2,7 @@ const { ApolloServer } = require('@apollo/server');
 const { typeDefs } = require('./typeDefs');
 const { resolvers } = require('./resolvers');
 
-module.exports.serverql = new ApolloServer({
+module.exports.serverql = new ApolloServer({//creo el server con los resolvers y los tipos.
   typeDefs,
   resolvers,
 });
