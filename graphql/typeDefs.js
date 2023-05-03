997ca0dada786262be6ce081707da0c2589fd05f
nicho100 <63875333+nicho100@users.noreply.github.com>
2023-05-03 17:04:32 -0300

    Update typeDefs.js

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,18 +1,18 @@
 module.exports.typeDefs = `#graphql
-  type User {//creo el tipo usuario que contiene un id,username y contraseña
+  type User {
     id: ID!
     username: String!
     password: String!
   }
-  input UserToAdd {//creo un input para despues insertarlo en la mutacion
+  input UserToAdd {
     username: String!
     password: String!
   }
-  type Query {//muestra un usuario cuando se ingrese el id
+  type Query {
     users: [User]!
     user(userId: ID!): User!
   }
-  type Mutation {//recibo el contenido de userToAdd y lo añado
+  type Mutation {
     addUser(userToAdd: UserToAdd!): User!
   }
 `;
