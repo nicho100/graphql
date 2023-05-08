1bdda50bb13a8673b62e15b77bfb01072f348b47
nicho100 <63875333+nicho100@users.noreply.github.com>
2023-05-08 14:32:14 -0300

    Update mutations.js

--- graphql/resolver/users/mutations.js
+++ graphql/resolver/users/mutations.js
@@ -1,6 +1,6 @@
 const { addUser } = require("../../../controller/user");
 const { ContenedorArchivo } = require('../../../controller/contenedorArchivos')
-const usuario= new ContenedorArchivo("productos")
+const usuario= new ContenedorArchivo("usuarios")
 module.exports.userMutations = {
   addUser: async (_, { userToAdd }) => await usuario.save(userToAdd),
 };
