6f86b5e71b3bdfefe69f270ec93ed618cc528ddb
nicho100 <63875333+nicho100@users.noreply.github.com>
2023-05-08 12:48:09 -0300

    Update queries.js

--- graphql/resolver/users/queries.js
+++ graphql/resolver/users/queries.js
@@ -1,6 +1,7 @@
 const { getUsers, getById } = require("../../../controller/user");
-
+const { ContenedorArchivo } = require('../../../controller/contenedorArchivos')
+const usuario= new ContenedorArchivo("productos")
 module.exports.userQueries = {
-  users: async () => await getUsers(),
-  user: async (_, { userId }) => await getById(userId),
+  users: async () => await usuario.getAll(),
+  user: async (_, { userId }) => await usuario.getbyid(userId),
 };
